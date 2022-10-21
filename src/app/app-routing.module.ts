729476085a9ee480200fdf2a7a00f3bae88fc986
Yann Attakoui <yannatkk@gmail.com>
2022-10-21 16:50:37 +0200

    remove unused modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from "@angular/fire/auth-guard";
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
+import { LandingPageComponent } from './modules/landing/pages/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(["landing"]);
+const redirectLoggedInToUsers = () => redirectLoggedInTo(["home"]);
+
+const routes: Routes = [{
+  component: LandingPageComponent,
+  path: "",
+  loadChildren: () => import("./modules/landing/landing.module").then((module) => module.LandingModule), ...canActivate(redirectLoggedInToUsers),
+}, {
+  component: HomePageComponent,
+  path: "home",
+  loadChildren: () => import("./modules/home/home.module").then((module) => module.HomeModule), ...canActivate(redirectUnauthorizedToLogin),
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
