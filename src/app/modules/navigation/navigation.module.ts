f601c54ea1c7a818dc9853946b42949f7a65b469
Yann Attakoui <yannatkk@gmail.com>
2022-11-03 08:54:09 +0100

    App bar

--- src/app/modules/navigation/navigation.module.ts
+++ src/app/modules/navigation/navigation.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ChatPageComponent } from './pages/chat-page/chat-page.component';
@@ -8,7 +9,6 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotificationsPageComponent } from './pages/notifications-page/notifications-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-
 
 
 @NgModule({
@@ -24,6 +24,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     CommonModule,
     MatIconModule,
     RouterModule,
+    MatToolbarModule,
   ],
   exports: [
 
