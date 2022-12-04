d2a8b334a4fb9e7547ae4e10e40724797cf54431
Yann Attakoui <yannatkk@gmail.com>
2022-12-04 12:10:47 +0100

    feat: display all post comments

--- src/app/modules/navigation/components/comment/comment.component.ts
+++ src/app/modules/navigation/components/comment/comment.component.ts
@@ -4,7 +4,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { collection, CollectionReference, DocumentData, Firestore } from '@angular/fire/firestore';
 import * as firebase from "firebase/firestore";
-import { Subject, takeUntil, tap } from 'rxjs';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
 import { FIREBASE_COLLECTION_PATHS } from 'src/app/constants/firestore-collection-paths.constant';
 import { PostComment } from 'src/app/models/post.comment.interface';
 import { AuthenticationService } from 'src/app/services/authentication.service';
@@ -16,14 +16,13 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 })
 export class CommentComponent implements OnInit, OnDestroy {
   private postCommentsCollection: CollectionReference<DocumentData>;
-  commentForm!: FormGroup;
-  createdAt: firebase.Timestamp = firebase.Timestamp.now();
-  comment: PostComment = { comment: '', userId: '', userFullname: '', createAt: this.createdAt };
-
+  private destroy$!: Subject<boolean>;
   currentUserFullname: any;
   currenUserId: any;
-
-  private destroy$!: Subject<boolean>;
+  commentForm!: FormGroup;
+  createdAt: firebase.Timestamp = firebase.Timestamp.now();
+  comments$!: Observable<PostComment[]>;
+  comment: PostComment = { comment: '', userId: '', userFullname: '', createAt: this.createdAt };
 
   constructor(
     public dialogRef: MatDialogRef<CommentComponent>,
@@ -37,6 +36,7 @@ export class CommentComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.comments$ = this.firestoreService.fetchAll<PostComment>(this.postCommentsCollection, 'createAt', 'asc');
     this.destroy$ = new Subject<boolean>();
     this.commentForm = this.formBuilder.group({
       comment: [null, [Validators.required, Validators.minLength(1)]],
@@ -55,13 +55,19 @@ export class CommentComponent implements OnInit, OnDestroy {
     this.destroy$.next(true);
   }
 
+  isCommentCreator(comment: PostComment): boolean {
+    if (comment.userId == this.currenUserId) {
+      return true;
+    }
+    return false;
+  }
+
   onComment(): void {
     this.comment.comment = this.commentForm.controls['comment'].value;
     this.comment.userFullname = this.currentUserFullname;
     this.comment.userId = this.currenUserId;
     this.firestoreService.create(this.postCommentsCollection, this.comment).then(
       () => this.commentForm.setValue({ comment: '' })
-    );
+    ).catch((error) => console.log(error));
   }
-
 }
