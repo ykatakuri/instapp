a63a0d7b518d381da53bfbbfd393b100a595b607
Yann Attakoui <yannatkk@gmail.com>
2022-12-04 18:31:42 +0100

    feat: add divider and grid list modules

--- src/app/modules/navigation/navigation.module.ts
+++ src/app/modules/navigation/navigation.module.ts
@@ -7,6 +7,8 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -42,7 +44,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     CreatePostFileComponent,
     PostComponent,
     PostListComponent,
-    CommentComponent
+    CommentComponent,
   ],
   imports: [
     CommonModule,
@@ -60,6 +62,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ReactiveFormsModule,
     MatDialogModule,
     MatProgressSpinnerModule,
+    MatGridListModule,
+    MatDividerModule,
   ],
   exports: [
 
